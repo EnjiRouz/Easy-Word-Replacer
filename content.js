c6e8f4510c35eaa43e53790c08ee866ad55f6a03
EnjiRouz <enji.rouz@yandex.ru>
2020-04-19 17:07:23 +0500

    Added checkbox to auto turn on saved replacing

--- content.js
+++ content.js
@@ -1,6 +1,29 @@
+// переменные, позволяющие хранить и перезаписывать найстройки пользователя в расширении
+let textToReplace = localStorage.getItem('textToReplace');
+let newText = localStorage.getItem('newText');
+let autoChangeText=localStorage.getItem('autoChangeText');
+
+// применение автоматической замены текста при открытии страницы (если включена соответствующая настройка)
+if(autoChangeText==="true")
+    replaceText(document.body, textToReplace, newText);
+
+/**
+ * Осуществление взаимодействия pop-up формы с background скриптом при помощи отправки-получения сообщений
+ * в активной вкладке с передачей в них необходимых для работы параметров.
+ * Переданные параметры перезаписывают предыдущие настройки
+ */
 chrome.runtime.onMessage.addListener(function (request,sender,sendResponse) {
     if (request.todo === "changeTexts") {
-        replaceText(document.body, request.ttp, request.nt);
+
+        textToReplace=request.ttp;
+        newText=request.nt;
+        autoChangeText=request.act;
+
+        localStorage.setItem('textToReplace', textToReplace);
+        localStorage.setItem('newText', newText);
+        localStorage.setItem('autoChangeText', autoChangeText);
+
+        replaceText(document.body, textToReplace, newText);
         sendResponse({
             response: "Message received"
         });
@@ -8,6 +31,12 @@ chrome.runtime.onMessage.addListener(function (request,sender,sendResponse) {
     }
 });
 
+/**
+ * Рекурсивно меняет содержимое дочерних элементов на заданное регулярным выражением (поиск осуществляется глобально, без учёта регистра)
+ * @param elementForSearchingIn - родительский элемент, в котором будет осуществляться поиск
+ * @param textToReplace - тект, который будет заменён
+ * @param newText - текст, на который старый текст будет заменён
+ */
 function replaceText(elementForSearchingIn, textToReplace, newText) {
     let regex = new RegExp(textToReplace, "gi");
 
--- popup.js
+++ popup.js
@@ -1,19 +1,25 @@
-let textToReplace = localStorage.getItem('textToReplace');
-let newText = localStorage.getItem('newText');
+// переменные, позволяющие хранить, применять при старте и перезаписывать найстройки пользователя в расширении
+let autoChangeTextLastChoice=localStorage.getItem('autoChangeTextLastChoice');
+document.getElementById("autoChangeText").checked = autoChangeTextLastChoice === "true";
 
+// явное указание события для кнопки (связано с политикой безопасности)
 let submitButton = document.getElementById("submit");
 if(submitButton) {
-    submitButton.addEventListener("click", changeTexts);
+    submitButton.addEventListener("click", saveSettings);
 }
 
-function changeTexts() {
-    textToReplace = document.getElementById("textToReplace").value;
-    newText = document.getElementById("newText").value;
+/**
+ * Сохранение настроек и применение их на странице.
+ * Применение настроек на странице происходит при помощи их передачи в сообщении, предназначенном для background скрипта
+ */
+function saveSettings() {
+    let textToReplace = document.getElementById("textToReplace").value;
+    let newText = document.getElementById("newText").value;
 
-    localStorage.setItem('textToReplace', textToReplace);
-    localStorage.setItem('newText', newText);
+    autoChangeTextLastChoice = document.getElementById("autoChangeText").checked;
+    localStorage.setItem('autoChangeTextLastChoice', autoChangeTextLastChoice);
 
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {todo: "changeTexts", ttp: textToReplace, nt: newText});
+        chrome.tabs.sendMessage(tabs[0].id, {todo: "changeTexts", ttp: textToReplace, nt: newText, act: autoChangeTextLastChoice});
     })
 }
